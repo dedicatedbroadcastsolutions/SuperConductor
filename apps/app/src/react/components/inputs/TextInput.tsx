ba600fcfd2da8cda623931311402afc98baa9ed7
zcybercomputing <zach.swena@gmail.com>
2026-02-16 23:28:50 +0000

    Fix TextInput template typing

--- apps/app/src/react/components/inputs/TextInput.tsx
+++ apps/app/src/react/components/inputs/TextInput.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react'
 import { ParsedValueInput } from './parsedValueInput.js'
 export const TextInput: React.FC<
 	| {
-			currentValue: string
+			currentValue: string | any // Allow template strings and other expression types
 			indeterminate?: boolean
 			onChange: (newValue: string) => void
 			allowUndefined: false
@@ -17,7 +17,7 @@ export const TextInput: React.FC<
 			focusTooltip?: string
 	  }
 	| {
-			currentValue: string | undefined
+			currentValue: string | undefined | any // Allow template strings and other expression types
 			indeterminate?: boolean
 			onChange: (newValue: string | undefined) => void
 			allowUndefined: true
